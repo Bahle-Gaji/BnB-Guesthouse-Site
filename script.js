b5960df02991d8c0e44e32a14e8a21f377af57f2
Bahle-Gaji <Bahle.gaji01@gmail.com>
2025-06-02 21:43:23 +0200

    Add comment

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Carousel scroll Function
 function scrollCarousel(direction) {
             const container = document.getElementById('carousel');
             const scrollAmount = 150; // Adjust based on item width + gap
